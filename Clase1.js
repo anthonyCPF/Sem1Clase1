76ffcd5b54e590ab00eb6147ca1e9fb0d84c5945
anthony <anthonys.x5x@gmail.com>
2021-05-22 02:36:09 -0500

    first commit

---
+++ Clase1.js
@@ -0,0 +1,26 @@
+// console.log('Hello world')
+// let d = new Date();
+// console.log("today's date is " + d);
+// document.body.innerHTML = "<h1>Today's date is" + d + "</h1>";
+// document.body.style.backgroundColor = "lightblue";
+// document.body.style.color = "white";
+
+// let p = document.createElement('p');
+// let t = document.createTextNode("this is a text");
+// p.appendChild(t)
+
+// document.body.appendChild(p);
+
+/**
+ *
+ */
+
+// let name = prompt("What's your name");
+// alert('hello, ' + name + '!');
+// alert(`hello ${name}`);
+
+const greeting = "hello";
+
+
+
+
